e32f2f64482871b1de63a6806a0e7dee3c793b89
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-04 13:06:15 +0000

    added `mui-theme-configuration`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
+import { createTheme } from "@mui/material/styles"
+import { useMemo } from "react"
+import { themeSettings } from "./theme"
+import { ThemeProvider } from "@emotion/react"
+import { CssBaseline } from "@mui/material"
 
 
 function App() {
 
+  const theme = useMemo(() => createTheme(themeSettings), [])
+
   return (
     <div className="app">
-      Rizvi Project
+      <ThemeProvider theme={theme}>
+        <CssBaseline /> {/* This is used to provide a consistent styling accross different browser -> Does by resetting default CSS styles */}
+      </ThemeProvider>
     </div>
   )
 }
