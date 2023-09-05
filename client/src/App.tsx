2586b6c7ec50b266ad710b8c1db0134d98cd8a40
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-05 06:30:48 +0000

    Created Navbar Component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { themeSettings } from "./theme"
 import { ThemeProvider } from "@emotion/react"
 import { CssBaseline } from "@mui/material"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import AppBar from "@/pages/appbar"
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
         <ThemeProvider theme={theme}>
           <CssBaseline /> {/* This is used to provide a consistent styling accross different browser -> Does by resetting default CSS styles */}
           <div style={{ width: "100%", height: "100%", padding: "1rem 2rem 4rem 2rem" }}> {/* Rem stands for Root em unit the unit is relative not absolute */}
+            <AppBar />
             <Routes>
               <Route path='/' element={<div>Dashbaord</div>} />
               <Route path='/predictions' element={<div>Predictions page</div>} />
--- client/src/pages/appbar/index.tsx
+++ client/src/pages/appbar/index.tsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Box, Typography, useTheme } from '@mui/material'
 type Props = {}
 
 function AppBar({ }: Props) {
+    const { palette } = useTheme();
     return (
-        <div>
-
-        </div>
+        <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', marginBottom: '0.25rem', padding: '0.5rem 0rem', color: palette.grey[300] }} >
+            Hello
+        </ div >
     )
 }
 
