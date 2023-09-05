37d5d7caed99422d51a3f4507a1312b354044ba6
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-05 06:07:09 +0000

    Added `routes`

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react"
 import { themeSettings } from "./theme"
 import { ThemeProvider } from "@emotion/react"
 import { CssBaseline } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 
 function App() {
@@ -11,10 +12,17 @@ function App() {
 
   return (
     <div className="app">
-      Rizvi Project
-      <ThemeProvider theme={theme}>
-        <CssBaseline /> {/* This is used to provide a consistent styling accross different browser -> Does by resetting default CSS styles */}
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline /> {/* This is used to provide a consistent styling accross different browser -> Does by resetting default CSS styles */}
+          <div style={{ width: "100%", height: "100%", padding: "1rem 2rem 4rem 2rem" }}> {/* Rem stands for Root em unit the unit is relative not absolute */}
+            <Routes>
+              <Route path='/' element={<div>Dashbaord</div>} />
+              <Route path='/predictions' element={<div>Predictions page</div>} />
+            </Routes>
+          </div>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ client/src/pages/appbar/index.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+type Props = {}
+
+function AppBar({ }: Props) {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AppBar
