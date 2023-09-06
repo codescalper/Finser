ba5ac382651851b3640cafe3c14de52bf13de700
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2023-09-06 11:14:11 +0000

    made it `responsive`

--- client/src/pages/dashbaord/index.tsx
+++ client/src/pages/dashbaord/index.tsx
@@ -1,8 +1,8 @@
-import { useTheme } from "@mui/material";
+import { useMediaQuery, useTheme } from "@mui/material";
 
 type Props = {}
 
-const gridTemplate = `
+const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
     "a b c"
@@ -14,12 +14,58 @@ const gridTemplate = `
     "g h j"
 `
 
+const gridTemplateSmallScreens = `
+    "a"
+    "a"
+    "a"
+    "a"
+    "b"
+    "b"
+    "b"
+    "b"
+    "c"
+    "c"
+    "c"
+    "d"
+    "d"
+    "d"
+    "d"
+    "e"
+    "e"
+    "f"
+    "f"
+    "f"
+    "g"
+    "g"
+    "g"
+    "h"
+    "h"
+    "h"
+    "i"
+    "i"
+    "j"
+    "j"
+`
+
 function Dashboard({ }: Props) {
+    const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
     const { palette } = useTheme();
 
+    // instead of cluterring the div style section wrote the logic here 
+    const gridStyles = isAboveMediumScreens ? {
+        gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+        gridTemplateRows: "repeat(10, minmax(60px, 1fr))",
+        gridTemplateAreas: gridTemplateLargeScreens,
+    }
+        : {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens,
+        }
     return (
-
-        <div style={{ width: "100%", height: '100%', display: "grid", gap: '1.5rem', gridTemplateAreas: gridTemplate, gridTemplateColumns: "repeat(3,minmax(370px,1fr))", gridTemplateRows: 'repeat(10,minmax(60px, 1fr))' }}>
+        <div style={{
+            width: "100%", height: '100%', display: "grid", gap: '1.5rem', ...gridStyles
+        }}>
             <div style={{ gridArea: 'a', backgroundColor: "#fff" }}></div>
             <div style={{ gridArea: 'b', backgroundColor: "#fff" }}></div>
             <div style={{ gridArea: 'c', backgroundColor: "#fff" }}></div>
@@ -30,8 +76,8 @@ function Dashboard({ }: Props) {
             <div style={{ gridArea: 'h', backgroundColor: "#fff" }}></div>
             <div style={{ gridArea: 'i', backgroundColor: "#fff" }}></div>
             <div style={{ gridArea: 'j', backgroundColor: "#fff" }}></div>
-        </div>
+        </div >
     )
 }
 
-export default Dashboard
+export default Dashboard;
