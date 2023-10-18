3772d86df44d51d5bc8b5d72df3730bba72727a4
codescalper <msfunbook@gmail.com>
2023-10-18 17:32:24 +0530

    Created Mongoose schema

--- server/index.js
+++ server/index.js
@@ -19,5 +19,23 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3000;
 
+mongoose.Promise = global.Promise; // Use native JavaScript promises
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(async () => {
+    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+    // await mongoose.connection.db.dropDatabase();
+    // KPI.insertMany(kpis);
+    // Product.insertMany(products);
+    // Transaction.insertMany(transactions);
+  })
+  .catch((error) => console.log(`${error} did not connect`));
+
 
 console.log("hello");
