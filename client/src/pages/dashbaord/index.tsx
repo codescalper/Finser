175ae77dc46ad4842df47e46def50f31cef134d8
codescalper <msfunbook@gmail.com>
2023-10-29 16:20:19 +0530

    Added secnod chart `Profit and revenue`

--- client/src/pages/dashbaord/index.tsx
+++ client/src/pages/dashbaord/index.tsx
@@ -1,6 +1,6 @@
 import { useMediaQuery, useTheme } from "@mui/material";
 import { useGetKpisQuery } from "@/state/api";
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Line, LineChart, Legend } from 'recharts';
 import { useMemo } from "react";
 import BoxHeader from "@/components/BoxHeader";
 
@@ -69,6 +69,19 @@ function Dashboard() {
         );
       }, [data]);
 
+      const revenueProfit = useMemo(() => {
+        return (
+          data &&
+          data[0].monthlyData.map(({ month, revenue, expenses }) => {
+            return {
+              name: month.substring(0, 3),
+              revenue: revenue,
+              profit: (revenue - expenses).toFixed(2),
+            };
+          })
+        );
+      }, [data]);
+
     const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
     // instead of cluterring the div style section wrote the logic here 
     const gridStyles = isAboveMediumScreens ? {
@@ -130,8 +143,17 @@ function Dashboard() {
                 />
               </linearGradient>
             </defs>
-          <XAxis dataKey="name" tickLine={false} style={{fontSize:"10px"}} axisLine={{strokeWidth:"0"}} domain={[8000,23000]} />
-          <YAxis />
+            <XAxis
+              dataKey="name"
+              tickLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              tickLine={false}
+              axisLine={{ strokeWidth: "0" }}
+              style={{ fontSize: "10px" }}
+              domain={[8000, 23000]}
+            />
           <Tooltip />
           <Area type="monotone" dot={true} dataKey="revenue" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorRevenue)" />
           <Area type="monotone" dot={true}  dataKey="expenses" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorExpenses)" />
@@ -139,7 +161,56 @@ function Dashboard() {
       </ResponsiveContainer>   
             </div>
             <div style={{ gridArea: 'b', backgroundColor: '#2d2d34', borderRadius: '1rem', boxShadow: '0.15rem 0.2rem 0.15rem 0.1rem rgba(0,0,0,0.8)' }}>
+            <BoxHeader
+          title="Profit and Revenue"
+          subtitle="top line represents revenue, bottom line represents expenses"
+          sideText="+4%"
+        />
+            <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={400}
+          data={revenueProfit}
+          margin={{
+            top: 20,
+              right: 0,
+              left: -10,
+              bottom: 55,
+          }}
+        >
+          <CartesianGrid vertical={false} stroke={palette.grey[800]} />
            
+          <XAxis 
+            dataKey="name" 
+            tickLine={false} 
+            style={{fontSize:"10px"}} 
+            axisLine={{strokeWidth:"0"}} 
+            domain={[8000,23000]} 
+          />
+          <YAxis
+              yAxisId="left"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+            <YAxis
+              yAxisId="right"
+              orientation="right"
+              tickLine={false}
+              axisLine={false}
+              style={{ fontSize: "10px" }}
+            />
+          <Tooltip />
+          <Legend
+              height={20}
+              wrapperStyle={{
+                margin: "0 0 10px 0",
+              }}
+            />
+          <Line  yAxisId="left" type="monotone" dataKey="profit" stroke={palette.tertiary[500]} />
+          <Line  yAxisId="right" type="monotone" dataKey="revenue" stroke={palette.primary.main} />
+        </LineChart>
+      </ResponsiveContainer>   
             </div>
             <div style={{ gridArea: 'c', backgroundColor: '#2d2d34', borderRadius: '1rem', boxShadow: '0.15rem 0.2rem 0.15rem 0.1rem rgba(0,0,0,0.8)' }}></div>
             <div style={{ gridArea: 'd', backgroundColor: '#2d2d34', borderRadius: '1rem', boxShadow: '0.15rem 0.2rem 0.15rem 0.1rem rgba(0,0,0,0.8)' }}></div>
