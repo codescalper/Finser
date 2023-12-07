2b18a298f5d80a84d21c35c77a589147b5c3ac99
codescalper <msfunbook@gmail.com>
2023-12-07 11:59:25 +0530

    Added campaign and target section in 2nd row

--- client/src/pages/dashbaord/index.tsx
+++ client/src/pages/dashbaord/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { useMediaQuery, useTheme } from "@mui/material";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
 import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
 import {
   AreaChart,
@@ -14,9 +14,18 @@ import {
   Legend,
   Bar,
   BarChart,
+  PieChart,
+  Pie,
+  Cell,
 } from "recharts";
 import { useMemo } from "react";
 import BoxHeader from "@/components/BoxHeader";
+import FlexBetween from "@/components/FlexBetween";
+
+const pieData = [
+  { name: "Group A", value: 600 },
+  { name: "Group B", value: 400 },
+];
 
 const gridTemplateLargeScreens = `
     "a b c"
@@ -66,6 +75,7 @@ const gridTemplateSmallScreens = `
 function Dashboard() {
   const { palette } = useTheme();
   const { data } = useGetKpisQuery();
+  const pieColors = [palette.primary[800], palette.primary[300]];
   const { data: operationalData } = useGetKpisQuery();
   const { data: productData } = useGetProductsQuery();
   console.log(operationalData);
@@ -364,6 +374,7 @@ function Dashboard() {
           title="Operational vs Non-Operational Expenses"
           sideText="+4%"
         />
+
         <ResponsiveContainer width="100%" height="100%">
           <LineChart
             data={operationalExpenses}
@@ -418,11 +429,51 @@ function Dashboard() {
           boxShadow: "0.15rem 0.2rem 0.15rem 0.1rem rgba(0,0,0,0.8)",
         }}
       >
-        <BoxHeader
-          title="Campaigns and targets"
-          // subtitle="top line represents revenue, bottom line represents expenses"
-          sideText="+4.12%"
-        />
+        <BoxHeader title="Campaigns and Targets" sideText="+4%" />
+        <FlexBetween mt="0.25rem" gap="1.5rem" pr="1rem">
+          <PieChart
+            width={110}
+            height={100}
+            margin={{
+              top: 0,
+              right: -10,
+              left: 10,
+              bottom: 0,
+            }}
+          >
+            <Pie
+              stroke="none"
+              data={pieData}
+              innerRadius={18}
+              outerRadius={38}
+              paddingAngle={2}
+              dataKey="value"
+            >
+              {pieData.map((entry, index) => (
+                <Cell key={`cell-${index}`} fill={pieColors[index]} />
+              ))}
+            </Pie>
+          </PieChart>
+          <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+            <Typography variant="h5">Target Sales</Typography>
+            <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>
+              83
+            </Typography>
+            <Typography variant="h6">
+              Finance goals of the campaign that is desired
+            </Typography>
+          </Box>
+          <Box flexBasis="40%">
+            <Typography variant="h5">Losses in Revenue</Typography>
+            <Typography variant="h6">Losses are down 25%</Typography>
+            <Typography mt="0.4rem" variant="h5">
+              Profit Margins
+            </Typography>
+            <Typography variant="h6">
+              Margins are up by 30% from last month.
+            </Typography>
+          </Box>
+        </FlexBetween>
       </div>
       <div
         style={{
