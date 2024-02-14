45b84f720203d9392b474443bbca1ac3b977ce4d
codescalper <msfunbook@gmail.com>
2024-02-14 10:45:13 +0530

    Added Predicition page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,41 @@
-import { createTheme } from "@mui/material/styles"
-import { useMemo } from "react"
-import { themeSettings } from "./theme"
-import { ThemeProvider } from "@emotion/react"
-import { CssBaseline } from "@mui/material"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import AppBar from "@/pages/appbar"
-import Dashboard from "@/pages/dashbaord"
-
+import { createTheme } from "@mui/material/styles";
+import { useMemo } from "react";
+import { themeSettings } from "./theme";
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppBar from "@/pages/appbar";
+import Dashboard from "@/pages/dashbaord";
+import Predictions from "./pages/predicition";
 
 function App() {
-
-  const theme = useMemo(() => createTheme(themeSettings), [])
+  const theme = useMemo(() => createTheme(themeSettings), []);
 
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
-          <CssBaseline /> {/* This is used to provide a consistent styling accross different browser -> Does by resetting default CSS styles */}
-          <div style={{ width: "100%", height: "100%", padding: "1rem 2rem 4rem 2rem" }}> {/* Rem stands for Root em unit the unit is relative not absolute */}
+          <CssBaseline />{" "}
+          {/* This is used to provide a consistent styling accross different browser -> Does by resetting default CSS styles */}
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              padding: "1rem 2rem 4rem 2rem",
+            }}
+          >
+            {" "}
+            {/* Rem stands for Root em unit the unit is relative not absolute */}
             <AppBar />
             <Routes>
-              <Route path='/' element={<Dashboard />} />
-              <Route path='/predictions' element={<div>Predictions page</div>} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/predictions" element={<Predictions />} />
             </Routes>
           </div>
         </ThemeProvider>
       </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
